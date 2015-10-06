8fb4519afa2f2a0469cd3a1b5b36fd4633c3ad34
Chetan Shenoy <me@chetanshenoy.com>
2015-10-06 17:26:52 -0400

    updated tests

--- test/index.js
+++ test/index.js
@@ -3,24 +3,43 @@ import assert from 'assert';
 import React from 'react/addons';
 const TestUtils = React.addons.TestUtils;
 
+const statesData = [
+  { label: 'Maryland', value: 'md' },
+  { label: 'Virginia', value: 'va' },
+  { label: 'Washington, DC', value: 'dc' }
+];
+
+const ageData = [
+  { text: 'Morning', value: 'morning' },
+  { text: 'Afternoon', value: 'afternoon' },
+  { text: 'Night', value: 'night' },
+  { text: 'All Day', value: 'allday' }
+];
+
 describe('Select component', () => {
   let renderedComponent;
 
-  before(() => {
-    const states = [
-      { label: 'Maryland', value: 'md' },
-      { label: 'Virginia', value: 'va' },
-      { label: 'Washington, DC', value: 'dc' }
-    ];
+  let createComponent = (dataset) => {
+    let instance = TestUtils.renderIntoDocument(
+      <Select value={'dc'} options={dataset} />
+    );
 
+    return instance;
+  };
+
+  before(() => {
     renderedComponent = TestUtils.renderIntoDocument(
-      <Select value={'dc'} options={states} />
+      <Select value={'dc'} options={statesData} />
     );
   });
 
-  it('Something', () => {
-    console.log(renderedComponent);
-    assert('a' === 'a');
+  it('Renders properly', () => {
+    // console.log(renderedComponent);
+    assert(renderedComponent);
+  });
+
+  it('Has properly selected default value', () => {
+    assert(renderedComponent.state.selectedOption.value === 'dc');
   });
 
 });
